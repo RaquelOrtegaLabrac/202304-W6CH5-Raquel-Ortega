3c0caf70ed759e56a6cb6e3c7c89e30582304c12
labrac <raquelortegalabrac@hotmail.com>
2023-06-10 14:11:39 +0200

    Add app.ts

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import createDebug from 'debug';
+const debug = createDebug('W6:App');
+
+export const app = express();
+
+debug('Loaded Express');
+const corsOptions = {
+  origin: '*',
+};
+
+app.use(morgan('dev'));
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use((_req, _res, next) => {
+  debug('Soy un middleware');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
+
+app.use('/data', dataRouter);
